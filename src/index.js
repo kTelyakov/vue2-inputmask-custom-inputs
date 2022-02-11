a10264b2f35baf711cf96c87b36d34fcb2786c26
Konstantin <telyakovkostya.learn@gmail.com>
2022-02-11 20:46:26 +0300

    (bug) handling input errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Inputmask from 'inputmask'
 
+
+var PREFIX = '[vue2-inputmask-custom-inputs]'
 var ERRORS = {
   NOT_EXIST: 'Не передано значение в директиву',
+  INPUTS_NOT_FOUND: 'Не найдено input[type="text"] внутри компонента'
 }
 
 function createError (e) {
-  console.warn(`[vue2-inputmask-custom-inputs]: ${e}`)
+  console.warn(`${PREFIX}: ${e}`)
 }
 
 function validateBindingValue (value) {
@@ -18,21 +21,28 @@ var inputmaskPlugin = {
     install: function(Vue, options) {
         Vue.directive('mask', {
             bind: function(el, binding) {
-              
+              try {
                 validateBindingValue(binding.value)
-
+              
                 if (el instanceof HTMLInputElement) {
                   Inputmask(binding.value).mask(el)
                   return
-                }
+                }    
 
                 // Если больше одного инпута содержится то ошибка должна быть
                 var nestedInput = el.querySelectorAll('input[type="text"]')
                 if (nestedInput && nestedInput.length > 1) {
-                  createError('Внутри компонента должен быть только 1 инпут')
+                  createError('Внутри обертки должно быть не больше одного input[type="text"] элемента')
+                  return
                 }
-                Inputmask(binding.value).mask(nestedInput[0])
-                
+                if (nestedInput && nestedInput[0] instanceof HTMLInputElement) {
+                  Inputmask(binding.value).mask(nestedInput[0])
+                }
+
+                createError(ERRORS.INPUTS_NOT_FOUND)
+              } catch (e) {
+                createError(e)
+              }
             }
         });
     }
